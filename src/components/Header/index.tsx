6c99d699ae11f2e5caea5f7cfe6c3865aeeba0d7
FernandaGabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2024-02-12 21:14:35 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,13 @@
-import { useSelector } from 'react-redux'
 import * as S from './styles'
 
-import { Produto } from '../../App'
-
 import cesta from '../../assets/cesta.png'
-import { praReal } from '../Produto'
+import { paraReal } from '../Produto'
+import { useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
 
 const Header = () => {
   const itens = useSelector((state: RootReducer) => state.carrinho.itens)
-  const itensFavoritos = useSelector(
-    (state: RootReducer) => state.favoritos.itens
-  )
+  const itensFav = useSelector((state: RootReducer) => state.favoritos.itens)
 
   const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
@@ -22,10 +18,10 @@ const Header = () => {
     <S.Header>
       <h1>EBAC Sports</h1>
       <div>
-        <span>{itensFavoritos.length} favoritos</span>
+        <span>{itensFav.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itens.length} itens, valor total: {praReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
